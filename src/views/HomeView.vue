5d43f3d0a5ae435f1777bf4c817d57da47c3b2c0
sunil <sunil.iiit15@gmail.com>
2024-07-15 21:53:35 +0530

    main : initial components for ride and food

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,24 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="Home" class="flex flex-col h-screen">
+    <div class="m-3 mb-auto">
+      <div class="grid grid-cols-2 gap-3 my-3">
+        <serviceSelectLarge 
+          text="Ride"
+          image="ride"
+          image-width="74"
+        />  
+
+        <serviceSelectLarge 
+          text="Food"
+          image="food"
+          image-width="72"
+          :promo="true"
+        />  
+      </div>
+    </div>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
+<script setup>
+  import serviceSelectLarge from '../components/ServiceSelectLarge.vue'
 </script>
